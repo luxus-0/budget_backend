08e143aadccb415a6207f43187d3f23c9d5f0d6b
nowog <luxus0@gmail.com>
2026-08-21 23:36:20 +0200

    Add class Transaction with field: id, amount, description, currency, date with method create with validation.

--- src/domain/models.py
+++ src/domain/models.py
@@ -1,3 +1,4 @@
+import uuid
 from dataclasses import dataclass, field
 from datetime import datetime, timezone
 from decimal import Decimal
@@ -11,3 +12,15 @@ class Transaction:
     description: str
     currency: str = 'USD'
     date: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
+
+    @classmethod
+    def create(cls, amount:Decimal, description:str, currency:str) -> 'Transaction':
+        if amount == Decimal("0.00") or amount == 0:
+            raise ValueError("Transaction amount cannot be zero.")
+        if not description.strip():
+            raise ValueError("Description cannot be empty")
+
+        return cls(id=uuid.uuid4(),
+                   amount=Decimal(str(amount)),
+                   description=description.strip(),
+                   currency=currency.upper())
