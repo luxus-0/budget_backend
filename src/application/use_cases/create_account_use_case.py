f3437d87efa6ad883266be0246ff32c86219a18d
nowog <luxus0@gmail.com>
2026-08-27 08:21:14 +0200

    refactor create account use case add method exists with name with check exception.

--- src/application/use_cases/create_account_use_case.py
+++ src/application/use_cases/create_account_use_case.py
@@ -1,6 +1,7 @@
 from application.commands.create_account_command import CreateAccountCommand
 from application.ports.account_repository import AccountRepository
 from domain.entities.account import Account
+from domain.exceptions import AccountAlreadyExistsError
 
 
 class CreateAccountUseCase:
@@ -8,12 +9,14 @@ class CreateAccountUseCase:
         self._account_repository = account_repository
 
     async def execute(self, command: CreateAccountCommand) -> Account:
+        if await self._account_repository.exists_with_name(command.user_id, command.name):
+            raise AccountAlreadyExistsError(command.name)
+
         account = Account.create(
             user_id=command.user_id,
             name=command.name,
             currency=command.currency,
-            kind=command.kind
+            kind=command.kind,
         )
-
         await self._account_repository.save(account)
         return account
